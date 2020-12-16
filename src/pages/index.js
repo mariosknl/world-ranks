396cd553df944d44f85623dafe4b80cf201e7c79
mariosknl <mariosknl@outlook.com>
2020-12-16 11:38:37 +0200

    sorting by name, region and subregion implemented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
 import Layout from "../components/Layout/Layout";
 import SearchInput from "../components/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
     <Layout>
       <p className={styles.counts}>Found {countries.length} countries</p>
 
-      <SearchInput placeholder="Filter by Name, Region, or SubRegion" />
+      <SearchInput
+        placeholder="Filter by Name, Region, or SubRegion"
+        onChange={onInputChange}
+      />
 
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
