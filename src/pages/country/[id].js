55f8e6ba10430731b50bc326334934cd47e68b7c
mariosknl <mariosknl@outlook.com>
2020-12-16 14:06:27 +0200

    updated imports

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
-import styles from "./Country.module.css";
+import styles from "./country.module.css";
 
 const getCountry = async (id) => {
   const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
